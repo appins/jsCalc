11ad8f9fb872259bba792535d297fd60eb40dfa9
Alex Anderson <alexandersonone@gmail.com>
2016-10-19 21:57:02 -0700

    Added addlast, clear, and delete

--- calc.js
+++ calc.js
@@ -13,25 +13,58 @@ module.exports.proc = function(arr){
     var res = 0;
 
     switch (lastIn.toUpperCase()) {
+      // Add all numbers together
       case "PLUS":
       case "ADD" :
       case "SUM" :
       case "+"   :
-        for(var i = 0; i < arr.length - 1; i++){
-          res += Number(arr[i]);
+      case "+ALL":
+        for(var addunit = 0; addunit < arr.length - 1; addunit++){
+          res += Number(arr[addunit]);
         }
         break;
 
+      // Multiply all numbers together
       case "MULTIPLY":
       case "TIMES"   :
       case "PRODUCT" :
       case "*"       :
         res = 1;
-        for(var j = 0; j < arr.length - 1; j++){
-          res *= Number(arr[j]);
+        for(var timeunit = 0; timeunit < arr.length - 1; timeunit++){
+          res *= Number(arr[timeunit]);
         }
         break;
 
+      // Add only the last two
+      case "ADDLAST" :
+      case "ADD2"    :
+      case "ADD 2"   :
+      case "SUMLAST" :
+      case "+LAST"   :
+        var temparr = [];
+        for(var addlast = 0; addlast < arr.length - 3; addlast++){
+          temparr[addlast] = arr[addlast];
+        }
+        temparr[temparr.length] = String(Number(arr[arr.length - 3]) + Number(arr[arr.length - 2]));
+        return temparr;
+
+
+      // Clear the array
+      case "CLEAR":
+        return [];
+
+      // Delte the last member (if there is one)
+      case "DEL"   :
+      case "DELTE" :
+        var delarr = [];
+        if(arr.length !== 0){
+          for(var del = 0; del < arr.length - 2; del++){
+            delarr[del] = arr[del];
+          }
+        }
+        return delarr;
+
+      // Exit the program
       case "EXIT" :
       case "QUIT" :
       case "CLOSE":
